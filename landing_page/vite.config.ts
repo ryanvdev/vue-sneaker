15cdb52462d1e883254418c292dd6ace6f4dc7ee
tuannguyen <tuannguyen7326.dev@gmail.com>
2023-09-27 13:55:01 +0700

    MODIFY - out dir for landing_page

--- landing_page/vite.config.ts
+++ landing_page/vite.config.ts
@@ -5,9 +5,12 @@ import path from 'node:path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: path.resolve(__dirname, '../docs/'),
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src/'),
     }
-  }
+  },
 });
